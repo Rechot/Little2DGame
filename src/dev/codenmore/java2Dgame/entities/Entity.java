58b733ddddd705c0fe7ef2f139046256b2cbc477
Piotr Kościuk <piotr.kosciuk87@gmail.com>
2019-08-28 14:44:44 +0200

    adding setters and getters to entity class

--- src/dev/codenmore/java2Dgame/entities/Entity.java
+++ src/dev/codenmore/java2Dgame/entities/Entity.java
@@ -5,10 +5,49 @@ import java.awt.*;
 public abstract class Entity {
 
     protected float x,y; //Why not int, because calculations in our game will not result in fixed numbers.
+    protected int width, height;
 
-    public Entity(float x, float y){
+    //Getters
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    //Setters
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Entity(float x, float y, int width, int height){
       this.x = x;
       this.y = y;
+      this.width = width;
+      this.height = height;
     }
 
     public abstract void tick();
