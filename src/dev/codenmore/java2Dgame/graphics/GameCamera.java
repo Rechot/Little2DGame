e3f6de26398607e179af4f84383b969c4e033436
Piotr Kościuk <piotr.kosciuk87@gmail.com>
2019-09-02 02:24:55 +0200

    camera system fix

--- src/dev/codenmore/java2Dgame/graphics/GameCamera.java
+++ src/dev/codenmore/java2Dgame/graphics/GameCamera.java
@@ -1,31 +1,38 @@
 package dev.codenmore.java2Dgame.graphics;
 
-import dev.codenmore.java2Dgame.Game;
+import dev.codenmore.java2Dgame.Handler;
 import dev.codenmore.java2Dgame.entities.Entity;
+import dev.codenmore.java2Dgame.tile.Tile;
+import org.jetbrains.annotations.NotNull;
 
 public class GameCamera {
 
     //InstanceFields
 
     private float xOffset, yOffset;
-    private Game game;
+    private float maxXOffset, maxYOffset;
+    private Handler handler;
 
     //Constructors
 
-    public GameCamera(float xOffset, float yOffset, Game game) {
+    public GameCamera(float xOffset, float yOffset, @NotNull Handler handler) {
         this.xOffset = xOffset;
         this.yOffset = yOffset;
-        this.game = game;
+        this.handler = handler;
+        maxXOffset = handler.getLevel().getLevelWidth()* Tile.TILE_WIDTH - handler.getGameWidth();
+        maxYOffset = handler.getLevel().getLevelHeight()* Tile.TILE_HEIGHT - handler.getGameHeight();
     }
 
     //Setters
 
     public void setxOffset(float xOffset) {
         this.xOffset = xOffset;
+        checkBlankSpace();
     }
 
     public void setyOffset(float yOffset) {
         this.yOffset = yOffset;
+        checkBlankSpace();
     }
 
     //Getters
@@ -43,10 +50,23 @@ public class GameCamera {
     public void move(float xAmt, float yAmt){
         this.xOffset += xAmt;
         this.yOffset += yAmt;
+        checkBlankSpace();
+    }
+
+    public void checkBlankSpace(){
+
+        if(xOffset < 0) { xOffset = 0; }
+        else if (xOffset > maxXOffset)
+           { xOffset = maxXOffset; }
+
+        if(yOffset < 0) { yOffset = 0; }
+        else if (yOffset > maxYOffset)
+            { yOffset = maxYOffset; }
     }
 
     public void centerOnEntity(Entity entity){
-        xOffset = entity.getX() - game.getWidth() / 2 + entity.getWidth() / 2 ;
-        yOffset = entity.getY() - game.getHeight() / 2 + entity.getHeight() / 2;
+        xOffset = entity.getX() - handler.getGameWidth() / 2 + entity.getWidth() / 2 ;
+        yOffset = entity.getY() - handler.getGameHeight() / 2 + entity.getHeight() / 2;
+        checkBlankSpace();
     }
 }
